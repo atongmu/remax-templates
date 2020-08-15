321dced3cb0136c2ca842e3b9f290fcd9ef272a6
atongmu <715636539@163.com>
2020-08-15 17:08:05 +0800

    搜索功能完善

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,35 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { View } from 'remax/wechat';
-import { Card, Loading, Button } from 'anna-remax-ui';
+import { Icon, SearchBar, Cell, Tag } from 'anna-remax-ui';
 
-import styles from './index.css';
-import { href } from '@/utils/common'
+import './index.less';
+import { href, toast } from '@/utils/common'
 
 export default () => {
-  const [isLoading, setLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
 
   useEffect(() => {
-    const setFun = setTimeout(() => {
-      setLoading(false)
-    }, 1500)
-    return () => {
-      setFun
-    }
-  }, [])
+    console.log('searchValeu')
+  }, [searchValue])
   return (
-    <View className={styles.app}>
-      <View className="padding-sm">
-        {isLoading ? (
-          <Loading />
-        ) : (
-            <Card>
-              <View className="padding-bottom-sm">
-                <Button look="anna" block onTap={() => href(``)}>商城</Button>
+    <View className="serach">
+      <View className="padding-sm bg-white">
+        <SearchBar
+          value={searchValue}
+          onInput={(e) => setSearchValue(e)}
+          onClear={() => setSearchValue('')}
+          onActionClick={() => setSearchValue('')}
+          onSubmit={() => toast("搜索")}
+          placeholder="搜索"
+          inputStyle={{
+            backgroundColor: '#d7f0db',
+          }}
+        />
+      </View>
+      <View className="padding-top-lg bg-white">
+        {searchValue === '' ? (
+          <View className="padding">
+            <View className="flex">
+              <View>搜索历史</View>
+              <View className="flex-sub text-right" onClick={() => toast("删除")}>
+                <Icon type="delete" color="#999" size="36rpx" />
               </View>
-              {/* <View className="padding-bottom-sm">
-                <Button look="warning" block onTap={() => href(``)}>库存管理</Button>
-              </View> */}
-            </Card>
+            </View>
+            <View className="padding-top">
+              <Tag color="blue">blue</Tag>
+              <Tag color="green">green</Tag>
+              <Tag color="yellow">yellow</Tag>
+              <Tag color="red">red</Tag>
+            </View>
+          </View>
+        ) : (
+            <View className="padding">
+              <View className="padding-bottom">
+                搜索： “{searchValue}”
+              </View>
+            </View>
           )}
       </View>
     </View>
