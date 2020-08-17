4cf293abdced47db971a0383bd469fe3317e4d26
zenos <715636539@163.com>
2020-08-17 15:02:11 +0800

    轮播图组件修改

--- src/components/swiper_model/index.tsx
+++ src/components/swiper_model/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Icon, Button } from 'anna-remax-ui';
 import { View, Swiper, SwiperItem, Image } from 'remax/wechat';
 
 export interface Props {
@@ -13,7 +12,7 @@ export interface Props {
 const SwiperModel = (props: Props) => {
     const { items, autoplay, indicatorDots, indicatorColor, indicatorActiveColor } = props
     return (
-        <View className="swiper" >
+        <View className="swiper">
             <Swiper autoplay={autoplay} indicatorDots={indicatorDots} indicatorActiveColor={indicatorActiveColor} indicatorColor={indicatorColor} circular={true}>
                 {items.map((item, index) => {
                     return (<SwiperItem key={index}>
