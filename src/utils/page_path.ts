862e441a0dad6d77b0f24cde7dadf021633a5efd
atongmu <715636539@163.com>
2020-08-18 08:17:23 +0800

    添加功能路由

--- src/utils/page_path.ts
+++ src/utils/page_path.ts
@@ -2,14 +2,14 @@
  * @Author: codingfly
  * @Description: 页面路径管理
  * @Date: 2020-08-13 09:56:53
- * @LastEditTime: 2020-08-13 20:41:24
+ * @LastEditTime: 2020-08-18 08:16:58
  * @FilePath: \remax-templates\src\utils\page_path.ts
  */
 const path = {
     // 首页
     home: '/pages/index/index',
     // 商城首页
-    mall: `/pages/mall/index`,
+    mall: '/pages/mall/index',
     // 库存管理首页
     cims: '/pages/cims/index',
     // 登录
@@ -24,5 +24,15 @@ const path = {
     order_list: '/pages/order_list/index',
     // 订单列表
     order_info: '/pages/order_info/index',
+    // 提交订单
+    order_submit: '/pages/order_submit/index',
+    // 地址
+    address_list: '/pages/address/index',
+    // 新增地址
+    address_edit: '/pages/address_edit/index',
+    // 支付成功
+    succeess: '/pages/succeess/index',
+    // 支付失败
+    error: '/pages/error/index',
 }
 export default path
