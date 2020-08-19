8a7392d94da2b8e0953796bbe64ff76ec2f2fadf
zenos <715636539@163.com>
2020-08-19 16:25:42 +0800

    完善

--- src/pages/order_submit/index.tsx
+++ src/pages/order_submit/index.tsx
@@ -1,37 +1,106 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'remax/wechat';
-import { Card, Loading, Button } from 'anna-remax-ui';
+import { View, Text, Image, getStorageSync } from 'remax/wechat';
+import { Cell, Icon, Button, Input } from 'anna-remax-ui';
 
-import styles from './index.css';
+import './index.less';
 import { href } from '@/utils/common'
+import PageLoading from '@/components/page_loading';
+import page_path from '@/utils/page_path';
 
+export interface item {
+  id: number,
+  name: string,
+  newPrice: string,
+  content: string,
+  changeGoods: { key: string, value: string, image: string },
+  sku: Array<{ key: string, value: string, image: string }>,
+  num: number
+}
 export default () => {
   const [isLoading, setLoading] = useState(true)
+  const [orderItems, setOrderItems] = useState<item[]>([])
 
   useEffect(() => {
     const setFun = setTimeout(() => {
+      init()
       setLoading(false)
     }, 1500)
     return () => {
       setFun
     }
   }, [])
+  const init = () => {
+    const cartItems = getStorageSync("cart")
+    if (cartItems) {
+      setOrderItems(item => JSON.parse(cartItems))
+    }
+  }
   return (
-    <View className={styles.app}>
-      <View className="padding-sm">
-        {isLoading ? (
-          <Loading />
-        ) : (
-            <Card>
-              <View className="padding-bottom-sm">
-                <Button look="anna" block onTap={() => href(``)}>商城</Button>
+    <View>
+      {isLoading ? (
+        <PageLoading color="#28a745" topVal="0" />
+      ) : (
+          <View className="margin-top-sm">
+            <View className="bg-white margin-bottom padding" style={{ position: 'relative' }}>
+              <View className="flex align-center">
+                <View className="flex-sub">
+                  <View><Text>小仙雨</Text><Text className="margin-left-sm">129******11</Text></View>
+                  <View>广东省深圳市南山区高新科技园中区一路</View>
+                </View>
+                <View>
+                  <Icon type="right" size="36" color="#999" />
+                </View>
               </View>
-              {/* <View className="padding-bottom-sm">
-                <Button look="warning" block onTap={() => href(``)}>库存管理</Button>
-              </View> */}
-            </Card>
-          )}
-      </View>
+              <View className="bg-img"></View>
+            </View>
+            <View className="bg-white">
+              <View className="padding solid-bottom">
+                商品信息
+              </View>
+              <View className="padding">
+                {orderItems && (
+                  orderItems.map((item, index) => {
+                    return (
+                      <View className="flex solid-bottom" key={index}>
+                        <View style={{ width: '180', height: '180' }}>
+                          <Image style={{ width: '100%', height: '100%' }} src={item.changeGoods.image} />
+                        </View>
+                        <View className="flex-sub margin-left-xs flex padding-tb-xs">
+                          <View className="flex-sub">
+                            <View>{item.name}</View>
+                            <View className="text-gray">sku</View>
+                          </View>
+                          <View className="text-right text-gray">
+                            <View><Text className="text-price">{item.newPrice}</Text></View>
+                            <View>x {item.num}</View>
+                          </View>
+                        </View>
+                      </View>
+                    )
+                  })
+                )}
+              </View>
+              <View>
+                <Cell label="商品总额"><Text className="text-price">111</Text></Cell>
+                <Cell label="优惠券" arrow><Text className="text-red">满5减1</Text></Cell>
+                <Cell label="发票" arrow><Text className="text-red">不开发票</Text></Cell>
+                <Cell label="配送费"><Text className="text-price">0/00</Text></Cell>
+                <Input label="订单备注" placeholder="选填：请先和商家协商一致" />
+                <Cell border={false}><View className="text-black">合计：<Text className="text-price text-red text-xl">111</Text></View></Cell>
+              </View>
+            </View>
+            <View style={{ position: 'fixed', bottom: '0', left: '0', width: '100%' }}>
+              <View className="flex align-center bg-white">
+                <View className="flex-sub text-center">
+                  实付金额：<Text className="text-price text-red text-xl">1111</Text>
+                </View>
+                <View className="padding-xs">
+                  <Button look="anna" onTap={() => href(page_path.succeess)}>确认支付宝</Button>
+                </View>
+              </View>
+            </View>
+          </View>
+        )}
     </View>
   );
 };
