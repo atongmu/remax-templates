6cc73707c4d03157638511a1f26b1e5cf1bdd97f
zenos <715636539@163.com>
2020-08-19 16:25:14 +0800

    完善

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'remax/wechat';
-import { Card, Loading, Button } from 'anna-remax-ui';
+import { View, reLaunch } from 'remax/wechat';
+import { Card, Button, Icon } from 'anna-remax-ui';
 
 import styles from './index.css';
 import { href } from '@/utils/common'
+import page_path from '@/utils/page_path';
 
 export default () => {
   const [isLoading, setLoading] = useState(true)
@@ -18,19 +19,28 @@ export default () => {
   }, [])
   return (
     <View className={styles.app}>
-      <View className="padding-sm">
-        {isLoading ? (
-          <Loading />
-        ) : (
-            <Card>
-              <View className="padding-bottom-sm">
-                <Button look="anna" block onTap={() => href(``)}>商城</Button>
-              </View>
-              {/* <View className="padding-bottom-sm">
-                <Button look="warning" block onTap={() => href(``)}>库存管理</Button>
-              </View> */}
-            </Card>
-          )}
+      <Card>
+        <View className="padding-bottom-sm">
+          <View className="text-center">
+            <Icon type="evaluate" size="120" color="#28a745" />
+          </View>
+          <View className="margin-top">
+            <View className="text-xxl">订单已支付成功</View>
+            <View className="text-gray padding-tb-xs">非常感谢您购买我们的产品</View>
+          </View>
+          <View className="flex margin-top">
+            <View className="flex-sub padding-lr-sm">
+              <Button block plain onTap={() => reLaunch({ url: page_path.mall })}>返回首页</Button>
+            </View>
+            <View className="flex-sub padding-lr-sm">
+              <Button type="primary" danger square block onTap={() => href(page_path.order_list)}>查看订单</Button>
+            </View>
+          </View>
+        </View>
+      </Card>
+      <View className="padding-xl">
+        <View className="padding-tb-xs">温馨提示:</View>
+        <View className="text-gray">付款成功后，请勿泄露银行卡号、手机验证码，否则会造成钱款损失！谨防电话诈骗！</View>
       </View>
     </View>
   );
