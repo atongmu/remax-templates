ecb3723def4560bc39960104d5f28be7a7b840af
zenos <715636539@163.com>
2020-08-20 16:11:32 +0800

    订单详情

---
+++ src/pages/order_detail/index.config.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: codingfly
+ * @Description: 订单详情
+ * @Date: 2020-08-18 08:06:44
+ * @LastEditTime: 2020-08-20 14:16:50
+ * @FilePath: \templates-ts\src\pages\order_detail\index.config.ts
+ */
+
+module.exports = {
+  navigationBarTitleText: '订单详情'
+};
