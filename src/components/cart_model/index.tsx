bf71ae6d2b966a551e67e23fbd998b8f3d95a3d2
zenos <715636539@163.com>
2020-08-20 17:02:32 +0800

    样式修改（未完成）

--- src/components/cart_model/index.tsx
+++ src/components/cart_model/index.tsx
@@ -21,20 +21,23 @@ const CartModel = (props: Props) => {
 
     return (
         <View className="cart-model flex">
-            <View style={{ width: '180', height: '180' }}>
+            <View>
+                <Icon type="round" />
+            </View>
+            <View style={{ width: '220', height: '220' }}>
                 <Image style={{ width: '100%', height: '100%' }} src={item.changeGoods.image} />
             </View>
-            <View className="flex-sub flex padding-tb-xs" onClick={detail}>
-                <View className="content flex-sub margin-lr-xs">
-                    <View className="title">{item.name}</View>
-                    <View className="bgt-gray padding-top-sm text-sm">
+            <View className="flex-sub flex" onClick={detail}>
+                <View className="flex-sub margin-lr-sm">
+                    <View className="title padding-bottom text-sm">{item.name}</View>
+                    <Text className="bg-gray padding-xs text-sm">
                         {item.changeGoods.value}
                         <Icon type="unfold" />
+                    </Text>
+                    <View className="flex padding-top">
+                        <View className="flex-sub text-red"><Text className="text-price">{item.newPrice}</Text></View>
+                        <View><Text>x</Text> {item.num}</View>
                     </View>
-                </View>
-                <View className="text-right text-gray">
-                    <View><Text className="text-price">{item.newPrice}</Text></View>
-                    <View>x {item.num}</View>
                 </View>
             </View>
         </View>
