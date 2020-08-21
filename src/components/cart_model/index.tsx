d74a7f82ac6feade8c4ce90d44b82f15ce30275b
zenos <715636539@163.com>
2020-08-21 15:54:27 +0800

    组件修改

--- src/components/cart_model/index.tsx
+++ src/components/cart_model/index.tsx
@@ -1,45 +1,51 @@
 import * as React from 'react';
-import { Icon, Stepper } from 'anna-remax-ui';
+import { Icon, SwipeAction, Checkbox } from 'anna-remax-ui';
 import { View, Text, Image } from 'remax/wechat';
 
 import './index.less'
 
 export interface Props {
     detail: () => void;
+    onChange?: () => void;
     item: {
         id: number,
         name: string,
         newPrice: string,
         content: string,
+        checked: boolean,
+        swiper: boolean,
         changeGoods: { key: string, value: string, image: string },
         num: number
     };
 }
 
 const CartModel = (props: Props) => {
-    const { item, detail } = props
-
+    const { item, onChange, detail } = props
     return (
-        <View className="cart-model flex">
-            <View>
-                <Icon type="round" />
-            </View>
-            <View style={{ width: '220', height: '220' }}>
-                <Image style={{ width: '100%', height: '100%' }} src={item.changeGoods.image} />
-            </View>
-            <View className="flex-sub flex" onClick={detail}>
-                <View className="flex-sub margin-lr-sm">
-                    <View className="title padding-bottom text-sm">{item.name}</View>
-                    <Text className="bg-gray padding-xs text-sm">
-                        {item.changeGoods.value}
-                        <Icon type="unfold" />
-                    </Text>
-                    <View className="flex padding-top">
-                        <View className="flex-sub text-red"><Text className="text-price">{item.newPrice}</Text></View>
-                        <View><Text>x</Text> {item.num}</View>
+        <View className="cart-model">
+            <SwipeAction open={item.swiper} options={[{ name: '删除', style: { backgroundColor: '#ff0000' }, onTap: () => console.log(1) }]}>
+                <View className="flex align-center">
+                    <View style={{ margin: '0 20rpx' }}>
+                        <Checkbox checked={item.checked} onChange={onChange} />
+                    </View>
+                    <View style={{ width: '220', height: '220' }}>
+                        <Image style={{ width: '100%', height: '100%' }} src={item.changeGoods.image} />
+                    </View>
+                    <View className="flex-sub flex" onClick={detail}>
+                        <View className="flex-sub margin-lr-sm">
+                            <View className="title margin-bottom text-sm">{item.name}</View>
+                            <Text className="bg-gray padding-xs text-sm">
+                                {item.changeGoods.value}
+                                <Icon type="unfold" />
+                            </Text>
+                            <View className="flex padding-top">
+                                <View className="flex-sub text-red"><Text className="text-price">{item.newPrice}</Text></View>
+                                <View><Text>x</Text> {item.num}</View>
+                            </View>
+                        </View>
                     </View>
                 </View>
-            </View>
+            </SwipeAction>
         </View>
     );
 };
