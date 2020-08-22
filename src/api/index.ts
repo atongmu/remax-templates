95e12ae849f964e1c609a875ce8bb52264c9dd91
zenos <715636539@163.com>
2020-08-22 16:58:28 +0800

    物料接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@
  * @Author: codingfly
  * @Description: 接口
  * @Date: 2020-07-30 17:27:03
- * @LastEditTime: 2020-08-20 10:39:00
+ * @LastEditTime: 2020-08-22 16:58:11
  * @FilePath: \templates-ts\src\api\index.ts
  */
 import { ajax } from '@/utils/common'
@@ -13,6 +13,10 @@ const api = {
     product_list: '/product_list',
     product_info: '/product_info',
     order_list: '/order_list',
+    materials: '/materials',
+    materials_in: '/materials_in',
+    materials_out: '/materials_out',
+    materials_detail: '/materials_detail',
 }
 export default api
 
@@ -39,4 +43,20 @@ export function getProduct(data: any) {
 //  获取分页订单
 export function getOrders(data: any) {
     return ajax(api.order_list, 'GET', data, false, true, true)
+}
+//  获取分页物料
+export function getMaterials(data: any) {
+    return ajax(api.materials, 'GET', data, false, true, true)
+}
+//  获取分页物料入库
+export function getMaterialsIn(data: any) {
+    return ajax(api.materials_in, 'GET', data, false, true, true)
+}
+//  获取分页物料出库
+export function getMaterialsOut(data: any) {
+    return ajax(api.materials_out, 'GET', data, false, true, true)
+}
+//  获取物料详情
+export function getMaterialsDetail(data: any) {
+    return ajax(api.materials_detail, 'GET', data, false, true, true)
 }
