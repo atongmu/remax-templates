607de50a2f9e5ffac44059e1f43c8163e6536d26
zenos <715636539@163.com>
2020-08-23 14:27:41 +0800

    更改分类按钮为选择显示

--- src/components/search_model/index.tsx
+++ src/components/search_model/index.tsx
@@ -6,21 +6,24 @@ import './index.less'
 
 export interface Props {
     text: string;
-    color: string;
-    sortFun: () => void;
+    color?: string;
+    showSort?: boolean;
+    sortFun?: () => void;
     searchFun: () => void;
 }
 
 const SearchModel = (props: Props) => {
-    const { text, color, sortFun, searchFun } = props
+    const { text, color, showSort, sortFun, searchFun } = props
 
     return (
         <View className="search flex align-center" style={{ background: color }}>
-            <View className="sort" onClick={sortFun}>
-                <View><Icon type="cascades" size="36" color="#ffffff" /></View>
-                <View className="text-white"><Text>分类</Text></View>
-            </View>
-            <View className="content flex-sub" onClick={searchFun}>
+            {showSort && (
+                <View className="sort" onClick={sortFun}>
+                    <View><Icon type="cascades" size="36" color="#ffffff" /></View>
+                    <View className="text-white"><Text>分类</Text></View>
+                </View>
+            )}
+            <View className={`flex-sub ${showSort?'content':'hideSortContent'}`} onClick={searchFun}>
                 <Text><Icon type="search_light" size="36" color="#aaaaaa" /></Text>
                 <Text className="text-gray padding-left-sm">{text}</Text>
             </View>
