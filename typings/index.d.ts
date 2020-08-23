744bb0c97dc663a2d29565ea7e81dc164bb79155
zenos <715636539@163.com>
2020-08-23 14:26:50 +0800

    添加原生loading

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,10 +2,11 @@
  * @Author: codingfly
  * @Description: 
  * @Date: 2020-08-13 08:54:44
- * @LastEditTime: 2020-08-22 08:40:57
+ * @LastEditTime: 2020-08-23 10:15:58
  * @FilePath: \templates-ts\typings\index.d.ts
  */
 declare module '*.png';
 declare module '*.css';
 declare module 'weui-miniprogram/miniprogram_dist/badge/badge'
 declare module 'weui-miniprogram/miniprogram_dist/slideview/slideview'
+declare module 'weui-miniprogram/miniprogram_dist/loading/loading'
