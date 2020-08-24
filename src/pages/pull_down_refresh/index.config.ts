9f76313f5ea49ce1dd230eaf84a39907df36055b
zenos <715636539@163.com>
2020-08-24 10:30:22 +0800

    功能完善

--- src/pages/pull_down_refresh/index.config.ts
+++ src/pages/pull_down_refresh/index.config.ts
@@ -2,11 +2,12 @@
  * @Author: codingfly
  * @Description: 下拉刷新
  * @Date: 2020-08-18 08:08:22
- * @LastEditTime: 2020-08-23 16:18:31
+ * @LastEditTime: 2020-08-24 08:14:20
  * @FilePath: \templates-ts\src\pages\pull_down_refresh\index.config.ts
  */
 
 module.exports = {
   navigationBarTitleText: '下拉刷新',
   enablePullDownRefresh: true,
+  onReachBottomDistance: 80
 };
--- src/pages/pull_down_refresh/index.tsx
+++ src/pages/pull_down_refresh/index.tsx
@@ -1,102 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo, useCallback, useRef } from 'react';
 import { View, Text } from 'remax/wechat';
-import { Loading } from 'anna-remax-ui';
+import { Cell } from 'anna-remax-ui';
 import { usePageEvent } from 'remax/macro';
 
 import './index.less'
 import { href, toast } from '@/utils/common'
-import usePullDownRefresh from '@/hooks/usePullDownRefresh'
+import { getMaterialsIn } from '@/api/index'
+import LoadingModel from '@/components/loading_model';
+import useReachBottom from '@/hooks/useReachBottom'
 
+export interface MaterialsItem {
+  id: number,
+  time: string,
+  num: number
+}
 export default () => {
-  const { pageLoading, setPageLoading } = usePullDownRefresh()
-  const [isLoading, setLoading] = useState(false)
+  const defaultDataSource = useRef(null)
+  const { initStatus, pageLoading, pageStatus, setInitStatus, setPageLoading, setPageStatus } = useReachBottom()
+  const [dataSource, setDataSource] = useState<MaterialsItem[]>([])
+  const [params, setParams] = useState({ method: 'GET' })
   usePageEvent('onPullDownRefresh', () => {
     // 可以返回一个 promise，控制何时停止下来刷新行为
+    setInitStatus(e => true)
     return new Promise((resolve) => {
       setTimeout(() => {
+        getDate()
         toast('刷新成功')
         resolve();
       }, 1000);
     })
   });
+  usePageEvent('onLoad', () => {
+    getDate()
+  });
+  usePageEvent('onReachBottom', () => {
+    if (pageStatus && pageLoading) {
+      console.log('onReachBottom')
+      getDate()
+    }
+  });
+  const reault = useCallback((data) => {
+    /**
+     * 传入默认选项
+     */
+    if (initStatus) {
+      console.log('data', data)
+      setDataSource(items => data);
+    } else {
+      setDataSource(items => dataSource.concat(data));
+    }
+  }, [dataSource])
+  const getDate = async () => {
+    setPageStatus(e => false)
+    const result: any = await getMaterialsIn({})
+    if (result.status === 200) {
+      if (result.data.length <= 20) {
+        setPageLoading(e => false)
+      }
+      reault(result.data)
+      setInitStatus(e => false)
+      setPageStatus(e => true)
+    }
+  }
   return (
     <View className="pull_down_refresh">
-      <View className="padding-sm solid-bottom">
-        <Text>订单编号：</Text><Text>9136257866</Text>
-      </View>
-      <View className="padding-sm solid-bottom">
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-        <View className="padding-tb-xl">下拉</View>
-      </View>
-      <View className="padding-sm text-center">
-        {isLoading ? (
-          <Loading type="wave" color="#1890FF" />
-        ) : (
-            <View className="no-more">
-              <Text className="title">没有更多了</Text>
-            </View>
-          )}
-      </View>
+      {dataSource.map((item: any, index: number) => (
+        <View key={index} className="padding-tb-xl">
+          <Cell key={index} label={item.num}>{item.time}</Cell>
+        </View>
+      ))}
+      <LoadingModel isLoading={pageLoading} />
     </View>
   );
 };
