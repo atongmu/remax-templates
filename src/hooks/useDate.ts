caaf2cc581c5e76fba6712516cf0e46bbac9365c
zenos <715636539@163.com>
2020-08-24 10:29:32 +0800

    自定义hooks修改

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -2,32 +2,35 @@
  * @Author: codingfly
  * @Description: 分页封装
  * @Date: 2020-08-18 15:27:27
- * @LastEditTime: 2020-08-23 14:52:34
- * @FilePath: \templates-ts\src\hooks\PullDownRefresh.ts
+ * @LastEditTime: 2020-08-24 09:16:55
+ * @FilePath: \templates-ts\src\hooks\useDate.ts
  */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ajax } from '@/utils/common'
 
 
-export default function useData() {
+export default function useData(url: string) {
     const [isLoading, setLoading] = useState(true);
+    const [query, setQuery] = useState<any>({});
     const [pageStatus, setPageStatus] = useState(true)
+    const [data, setData] = useState<any>();
     const [isError, setError] = useState(false)
-    const func = async (api: string, options: any = {}) => {
-        let result: any = null
-        setPageStatus(pageStatus => pageStatus = false)
-        try {
-            let res: any = await ajax(api, options.method, options.data, options.isDelay, options.isForm, options.hideLoad)
-            console.log('data', res.data)
-            if (res.code == 200) {
-                result = res.data
+    useEffect(() => {
+        setPageStatus(e => false)
+        async function fetchData() {
+            try {
+                let res: any = await ajax(url, query.method, query.data, query.isDelay, query.isForm, query.hideLoad)
+                console.log(res)
+                if (res.status == '200') {
+                    setData(res.data)
+                }
+                setPageStatus(e => true)
+                setLoading(false)
+            } catch (err) {
+                setError(true)
             }
-            setPageStatus(pageStatus => pageStatus = true)
-        } catch (err) {
-            setPageStatus(pageStatus => pageStatus = true)
-            setError(true)
         }
-        return result
-    }
-    return { isLoading, isError, pageStatus, setLoading, func }
+        fetchData()
+    }, [query])
+    return { isLoading, isError, pageStatus, data, setQuery }
 }
---
+++ src/hooks/useReachBottom.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: codingfly
+ * @Description: 上拉加载
+ * @Date: 2020-08-24 09:33:02
+ * @LastEditTime: 2020-08-24 09:51:54
+ * @FilePath: \templates-ts\src\hooks\useReachBottom.ts
+ */
+
+import React, { useState, useEffect } from 'react'
+import { toast } from '../utils/common';
+
+export default function useReachBottom() {
+    const [pageLoading, setPageLoading] = useState(true);
+    const [pageStatus, setPageStatus] = useState(true)
+    const [initStatus, setInitStatus] = useState(true)
+    useEffect(() => {
+        if (!pageLoading && !initStatus) {
+            toast("没有更多数据了")
+        }
+    }, [pageLoading])
+    return { initStatus, pageLoading, pageStatus, setInitStatus, setPageLoading, setPageStatus }
+}
