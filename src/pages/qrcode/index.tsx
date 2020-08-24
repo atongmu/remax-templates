fccf536b8f9176f08f384dc7bed49c1904aa1ea6
zenos <715636539@163.com>
2020-08-24 15:13:48 +0800

    去掉无用代码

--- src/pages/qrcode/index.tsx
+++ src/pages/qrcode/index.tsx
@@ -2,24 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { View } from 'remax/wechat';
 
 import QrcodeModel from '@/components/qrcode_model';
-import PageLoading from '@/components/page_loading';
 
 export default () => {
-  const [isLoading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const setFun = setTimeout(() => {
-      setLoading(false)
-    }, 1000)
-    return () => {
-      setFun
-    }
-  }, [])
   return (
     <View className="pull_down_refresh">
-      {isLoading && (
-        <PageLoading color="#28a745" topVal="0" />
-      )}
       <View className="text-center">
         <QrcodeModel text="1111" />
       </View>
