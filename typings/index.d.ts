9be7afa84783e7aaca7fe9bd86c577663a144978
zenos <715636539@163.com>
2020-08-29 15:52:16 +0800

    去掉不用的组件

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,14 +2,12 @@
  * @Author: codingfly
  * @Description: 
  * @Date: 2020-08-13 08:54:44
- * @LastEditTime: 2020-08-29 10:29:31
+ * @LastEditTime: 2020-08-29 15:50:39
  * @FilePath: \templates-ts\typings\index.d.ts
  */
 declare module '*.png';
 declare module '*.css';
-declare module 'weui-miniprogram/miniprogram_dist/badge/badge'
 declare module 'weui-miniprogram/miniprogram_dist/slideview/slideview'
 declare module 'weui-miniprogram/miniprogram_dist/loading/loading'
 declare module 'weui-miniprogram/miniprogram_dist/toptips/toptips'
-declare module 'weui-miniprogram/miniprogram_dist/navigation-bar/navigation-bar'
-declare module 'weui-miniprogram/miniprogram_dist/gallery/gallery'
+declare module 'weui-miniprogram/miniprogram_dist/searchbar/searchbar'
