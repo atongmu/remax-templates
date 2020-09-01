859d2e2ab5804d95d08752eac7c97e1fab9a2fc1
zenos <715636539@163.com>
2020-09-01 16:31:35 +0800

    修改标题

--- src/pages/cart/index.config.ts
+++ src/pages/cart/index.config.ts
@@ -2,10 +2,10 @@
  * @Author: codingfly
  * @Description: 购物车
  * @Date: 2020-08-15 17:22:33
- * @LastEditTime: 2020-08-15 17:23:07
- * @FilePath: \remax-templates\src\pages\cart\index.config.ts
+ * @LastEditTime: 2020-09-01 16:31:26
+ * @FilePath: \templates-ts\src\pages\cart\index.config.ts
  */
 
 module.exports = {
-  navigationBarTitleText: '购物车'
+  navigationBarTitleText: '篮子'
 };
