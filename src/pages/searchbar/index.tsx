6689aee1f292b35e0bf1272f6ab90ab9eeaa4cf6
zenos <715636539@163.com>
2020-09-04 16:14:43 +0800

    添加点击事件

--- src/pages/searchbar/index.tsx
+++ src/pages/searchbar/index.tsx
@@ -4,7 +4,8 @@ import SearchbarModel from '@/components/searchbar_model';
 import SearchModel from '@/components/search_model';
 import useRefState from '@/hooks/useRefState'
 
-import { toast } from '@/utils/common'
+import { toast, href } from '@/utils/common'
+import page_path from '@/utils/page_path';
 export default () => {
   const [searchValue, setSearchValue, searchRef] = useRefState('')
   const handleValue = useCallback((e) => {
@@ -20,7 +21,7 @@ export default () => {
         <SearchbarModel value={searchValue} onInput={(e) => handleValue(e)} onActionClick={cancel} onClear={() => handleValue('')} />
       </View>
       <View className="padding-tb">
-        <SearchModel text='搜索' showSort={true} color="#28a745" searchFun={() => toast("搜索")} sortFun={() => toast("分类")} />
+        <SearchModel text='搜索' showSort={true} color="#28a745" searchFun={() => href(page_path.search)} sortFun={() => href(page_path.sort)} />
       </View>
     </View>
   );
