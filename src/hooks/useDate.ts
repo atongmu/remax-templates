daab5c95660aba9f7a34a8f1ff035ca5c98d63f1
zenos <715636539@163.com>
2020-09-04 16:15:02 +0800

    改进逻辑

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -2,12 +2,14 @@
  * @Author: codingfly
  * @Description: 分页封装
  * @Date: 2020-08-18 15:27:27
- * @LastEditTime: 2020-09-04 15:11:07
+ * @LastEditTime: 2020-09-04 15:59:32
  * @FilePath: \templates-ts\src\hooks\useDate.ts
  */
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { ajax } from '@/utils/common'
+import useRefState from '@/hooks/useRefState'
 export interface Props {
+    pageSize: number,
     url: string,
     method: string,
     data?: any,
@@ -16,25 +18,21 @@ export interface Props {
     hideLoad?: boolean,
 }
 
-export default ({ url, method, data, isDelay, isForm, hideLoad }: Props) => {
+export default ({ url, method, data, isDelay, isForm, hideLoad, pageSize = 20 }: Props) => {
     const [isLoading, setLoading] = useState<boolean>(true);
     const [pageStatus, setPageStatus] = useState<boolean>(true)
     const [params, setParams] = useState<any>()
+    const [hasMore, setHasMore, hasMoreRef] = useRefState(true)
     const [list, setList] = useState<any[]>([])
     const [isError, setError] = useState<boolean>(false)
-    useEffect(() => {
-        setParams((o: any) => o = data)
-    }, [data])
-    useEffect(() => {
-        setPageStatus(o => o = false)
+
+    const getData = useCallback(() => {
         const fetchData = async () => {
             try {
-                let res: any = await ajax(url, method, params, isDelay, isForm, hideLoad)
-                if (res.status == '200') {
-                    setList(o => o = res.data)
+                let res: any = await ajax(url, method, data, isDelay, isForm, hideLoad)
+                if (res.status === 200) {
+                    return res.data
                 }
-                setPageStatus(o => o = true)
-                setLoading(o => o = false)
             } catch (err) {
                 setError(o => o = true)
             }
@@ -42,6 +40,22 @@ export default ({ url, method, data, isDelay, isForm, hideLoad }: Props) => {
         return () => {
             fetchData()
         }
-    }, [params])
-    return { isLoading, isError, pageStatus, list, setParams }
+    }, [])
+    const load = useCallback(async () => {
+        if (!hasMoreRef.current) {
+            return
+        }
+        const res: any = await getData()
+        if (res.length < pageSize) {
+            setHasMore(false)
+        }
+        setList(l => {
+            if (res.length === 0 && l.length === 0) {
+                setError(true)
+            }
+
+            return [...l, ...res]
+        })
+    }, [])
+    return { isLoading, isError, pageStatus, list, setParams, load }
 }
