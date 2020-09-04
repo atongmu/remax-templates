4cf262633154bbbf38729368fea3b042a5778044
zenos <715636539@163.com>
2020-09-04 15:11:53 +0800

    修改请求数据方法

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -2,35 +2,46 @@
  * @Author: codingfly
  * @Description: 分页封装
  * @Date: 2020-08-18 15:27:27
- * @LastEditTime: 2020-08-24 09:16:55
+ * @LastEditTime: 2020-09-04 15:11:07
  * @FilePath: \templates-ts\src\hooks\useDate.ts
  */
 import React, { useState, useEffect } from 'react'
 import { ajax } from '@/utils/common'
+export interface Props {
+    url: string,
+    method: string,
+    data?: any,
+    isDelay?: boolean,
+    isForm?: boolean,
+    hideLoad?: boolean,
+}
 
-
-export default function useData(url: string) {
-    const [isLoading, setLoading] = useState(true);
-    const [query, setQuery] = useState<any>({});
-    const [pageStatus, setPageStatus] = useState(true)
-    const [data, setData] = useState<any>();
-    const [isError, setError] = useState(false)
+export default ({ url, method, data, isDelay, isForm, hideLoad }: Props) => {
+    const [isLoading, setLoading] = useState<boolean>(true);
+    const [pageStatus, setPageStatus] = useState<boolean>(true)
+    const [params, setParams] = useState<any>()
+    const [list, setList] = useState<any[]>([])
+    const [isError, setError] = useState<boolean>(false)
     useEffect(() => {
-        setPageStatus(e => false)
-        async function fetchData() {
+        setParams((o: any) => o = data)
+    }, [data])
+    useEffect(() => {
+        setPageStatus(o => o = false)
+        const fetchData = async () => {
             try {
-                let res: any = await ajax(url, query.method, query.data, query.isDelay, query.isForm, query.hideLoad)
-                console.log(res)
+                let res: any = await ajax(url, method, params, isDelay, isForm, hideLoad)
                 if (res.status == '200') {
-                    setData(res.data)
+                    setList(o => o = res.data)
                 }
-                setPageStatus(e => true)
-                setLoading(false)
+                setPageStatus(o => o = true)
+                setLoading(o => o = false)
             } catch (err) {
-                setError(true)
+                setError(o => o = true)
             }
         }
-        fetchData()
-    }, [query])
-    return { isLoading, isError, pageStatus, data, setQuery }
+        return () => {
+            fetchData()
+        }
+    }, [params])
+    return { isLoading, isError, pageStatus, list, setParams }
 }
