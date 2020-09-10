5f75806e8e5fe7b55a7e5ebc1ec9262c0e12b38f
zenos <715636539@163.com>
2020-09-10 16:22:23 +0800

    修改顶部标题

--- src/pages/goods_sort/index.config.ts
+++ src/pages/goods_sort/index.config.ts
@@ -2,10 +2,10 @@
  * @Author: codingfly
  * @Description: 商品分类
  * @Date: 2020-08-13 20:32:23
- * @LastEditTime: 2020-08-30 07:25:01
- * @FilePath: \remax-templates\src\pages\sort\index.config.ts
+ * @LastEditTime: 2020-09-10 16:22:01
+ * @FilePath: \templates-ts\src\pages\goods_sort\index.config.ts
  */
 
 module.exports = {
-  navigationBarTitleText: '分类'
+  navigationBarTitleText: '模板'
 };
