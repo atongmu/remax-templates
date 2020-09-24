b454a511507519308388f2b447a102dfd0e6b47d
atongmu <715636539@163.com>
2020-09-24 22:22:14 +0800

    添加默认值

--- src/components/page_loading/index.tsx
+++ src/components/page_loading/index.tsx
@@ -6,13 +6,13 @@ import './index.less'
 
 export interface Props {
     color?: string;
-    topVal?: string;
+    topVal?: number;
 }
 
-const PageLoading = ({ color, topVal }: React.PropsWithChildren<Props>) => {
+const PageLoading = ({ color="28a745", topVal=0 }: React.PropsWithChildren<Props>) => {
 
     return (
-        <View className="loading" style={{ top: topVal }}>
+        <View className="loading" style={{ top: `${topVal}px` }}>
             <View className="content">
                 <Loading type="anna" color={color} />
             </View>
