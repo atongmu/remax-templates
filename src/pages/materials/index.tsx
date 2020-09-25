147cae49afde3f9ae4336c91bc6e3e806bb298a8
zenos <715636539@163.com>
2020-09-25 09:31:15 +0800

    完善页面功能

--- src/pages/materials/index.tsx
+++ src/pages/materials/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { showLoading, hideLoading } from 'remax/wechat';
 import { View } from 'remax/one';
 import { usePageEvent } from 'remax/macro';
@@ -7,9 +7,10 @@ import { Cell } from 'anna-remax-ui';
 import { href, toast } from '@/utils/common'
 import page_path from '@/utils/page_path';
 import SearchModel from '@/components/search_model/index';
-import PageLoading from '@/components/page_loading';
-import { getMaterials } from '@/api/index'
+import LoadingModel from '@/components/loading_model';
 import { TodoContext } from '@/app';
+import useData from '@/hooks/useData'
+import { deepClone } from '../../utils/util';
 
 export interface MaterialsItem {
   id: number,
@@ -18,58 +19,64 @@ export interface MaterialsItem {
   num: number
 }
 export default () => {
-  const [isLoading, setLoading] = useState(true)
-  const [materials, setMaterials] = useState<MaterialsItem[]>([])
   const todo: any = React.useContext(TodoContext);
+  const [params, setParams] = useState({
+    page_no: 1,
+    page_size: 20,
+    name: todo.bingItems.searchValue
+  })
+  const { pageStatus, empty, hasMore, list, load, clean } = useData<MaterialsItem>({
+    url: '/materials'
+  })
   usePageEvent('onPullDownRefresh', () => {
     // 可以返回一个 promise，控制何时停止下来刷新行为
     return new Promise((resolve) => {
       setTimeout(() => {
-        showLoading({ title: "努力加载中", mask: true })
-        refresh()
+        const new_params = deepClone(params)
+        clean()
+        setParams({ ...new_params, page_no: params.page_no + 1 })
         resolve();
-      }, 100000000);
+      }, 100);
     })
+  });
+  usePageEvent('onReachBottom', () => {
+    if (pageStatus && hasMore) {
+      const new_params = deepClone(params)
+      setParams({ ...new_params, page_no: params.page_no + 1 })
+    }
   });
   useEffect(() => {
     init()
-  }, [])
-  const init = () => {
-    const setFun = setTimeout(() => {
-      getData()
-      setLoading(false)
-    }, 1500)
+  }, [params])
+  useEffect(() => {
+    const setFc = setTimeout(() => {
+      const new_params = deepClone(params)
+      clean()
+      setParams({ ...new_params, page_no: 1, name: todo.bingItems.searchValue })
+    }, 500)
     return () => {
-      setFun
+      clearTimeout(setFc)
     }
+  }, [todo.bingItems.searchValue])
+  const init = () => {
+    const new_params = deepClone(params)
+    load(new_params)
   }
-  const refresh = () => {
-    const setFun = setTimeout(() => {
-      hideLoading()
-    }, 1500)
-  }
-  const getData = async () => {
-    const materialsResult: any = await getMaterials({})
-    if (materialsResult.status === 200) {
-      setMaterials(materialsResult.data)
-    }
-  }
+  const Loading = useMemo(() => <LoadingModel isLoading={hasMore} empty={empty} />, [hasMore]);
   return (
     <View className="materials padding-env">
       <View className="nav fixed">
-        <SearchModel text={`${todo.bingItems.searchValue !== '' ? todo.bingItems.searchValue : '搜索物料'}`} searchFun={() => toast("搜索")} color="#28a745" />
+        <SearchModel text={`${params.name !== '' ? params.name : '搜索物料'}`} searchFun={() => href(page_path.search)} color="#28a745" />
       </View>
-      {isLoading && (
-        <PageLoading color="#28a745" topVal="90rpx" />
-      )}
       <View style={{ height: '100', background: '#28a745' }}></View>
       <View>
-        {materials.map((item, index) => (
+        {list.map((item, index) => (
           <Cell key={index} label={item.name} arrow onTap={() => toast("详情")}>
             <View>库存：{item.num}</View>
           </Cell>
         ))}
       </View>
+      {Loading}
     </View>
   );
 };
